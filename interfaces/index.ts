c04a76339d9430bfb3325c2c0976af7369d1aa8a
dijez <khadijaahmad2k21@gmail.com>
2025-11-20 13:19:28 +0100

    update

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -9,3 +9,25 @@ export interface CardProps {
   title: string;
   description: string;
 }
+export interface PillProps{
+  label: string
+}
+
+export interface PropertyProps{
+  name: string,
+    address: {
+      state: string,
+      city: string,
+      country: string
+    },
+    rating: number,
+    category: [string],
+    price: number,
+    offers: {
+      bed: string,
+      shower: string,
+      occupants: string
+    },
+    image: string,
+    discount: string,
+}
